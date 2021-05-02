6508da4d0b5d7fa269b930e67d95f70117ee044c
toundaherve <toundaherve@gmail.com>
2021-05-02 13:57:52 +0100

    Add Notification Feature

--- email-service/index.js
+++ email-service/index.js
@@ -1,16 +1,37 @@
 const sgMail = require("@sendgrid/mail")
 
-function notifyNewOfferToBuyer() {
+function notifyNewOfferToBuyer(buyer, seller, item) {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     const msg = {
-        to: "toundaherve@gmail.com",
+        to: buyer.email,
         from: {email:"notifications@bonvih.com", name: "Bonvih"},
         replyTo: "customer-service@bonvih.com",
-        fromname: "Bonvih",
-        subject: "New offer for your Iphone X need",
-        text: "Hi Herve, there is a new offer."
+        template_id: "d-fd32e146aa294990b70237b89d3fe7a2",
+        dynamic_template_data: {
+            buyer,
+            seller,
+            item
+        }
     }
     sgMail.send(msg).then(() => console.log("Email sent")).catch(error => console.log(error))
 }
 
-notifyNewOfferToBuyer()
+const buyer = {
+    name: "Herve",
+    email: "toundaherve@gmail.com", 
+    budget: "$3000",
+}
+
+const item = {
+    name: "Toyota Corolla",
+    images:[]
+}
+
+const seller = {
+    name: "Franck",
+    email: "franckbethuel@gmail.com",
+    city: "Dudley",
+    country: "United Kingdom"
+}
+
+notifyNewOfferToBuyer(buyer, seller, item)
